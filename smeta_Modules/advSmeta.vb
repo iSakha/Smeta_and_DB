7247790420573696dcbf07fe9dcaccdfc41e88f6
sakha60@gmail.com <sakha60@gmail.com>
2020-09-18 16:56:55 +0300

    Format working fine,but need to improve.Status OK

--- smeta_Modules/advSmeta.vb
+++ smeta_Modules/advSmeta.vb
@@ -69,7 +69,7 @@
                     mainForm.companyDGV(i).Rows.Add(row)
                 End If
             Next
-            'format_advanced_smeta(mainForm.companyDGV(i))
+            format_companyDGV(mainForm.companyDGV(i))
         Next
     End Sub
 
@@ -79,4 +79,165 @@
         Next
     End Sub
 
+    Sub format_companyDGV(_DGV)
+
+        Dim NRFormat As String = "### ### ##0"
+        _DGV.RowHeadersVisible = False
+        _DGV.Columns(0).Visible = False         ' Dep
+        _DGV.Columns(1).Visible = False         ' Cat
+        _DGV.Columns(2).Visible = False         ' ID
+
+        For Each r As DataGridViewRow In _DGV.Rows
+            Select Case r.Cells(0).Value
+                Case 1
+                    r.Cells(0).Style.BackColor = Color.LemonChiffon
+                    If odd_even(r.Cells(1).Value) Then
+                        r.Cells(1).Style.BackColor = Color.LemonChiffon
+                        r.Cells(2).Style.BackColor = Color.LemonChiffon
+                    End If
+                Case 2
+                    r.Cells(0).Style.BackColor = Color.LightSteelBlue
+                    If odd_even(r.Cells(1).Value) Then
+                        r.Cells(1).Style.BackColor = Color.LightSteelBlue
+                        r.Cells(2).Style.BackColor = Color.LightSteelBlue
+                    End If
+                Case 3
+                    r.Cells(0).Style.BackColor = Color.MistyRose
+                    If odd_even(r.Cells(1).Value) Then
+                        r.Cells(1).Style.BackColor = Color.MistyRose
+                        r.Cells(2).Style.BackColor = Color.MistyRose
+                    End If
+                Case 4
+                    r.Cells(0).Style.BackColor = Color.Honeydew
+                    If odd_even(r.Cells(1).Value) Then
+                        r.Cells(1).Style.BackColor = Color.Honeydew
+                        r.Cells(2).Style.BackColor = Color.Honeydew
+                    End If
+                Case 5
+                    r.Cells(0).Style.BackColor = Color.LightCyan
+                    If odd_even(r.Cells(1).Value) Then
+                        r.Cells(1).Style.BackColor = Color.LightCyan
+                        r.Cells(2).Style.BackColor = Color.LightCyan
+                    End If
+                Case 6
+                    r.Cells(0).Style.BackColor = Color.Thistle
+                    If odd_even(r.Cells(1).Value) Then
+                        r.Cells(1).Style.BackColor = Color.Thistle
+                        r.Cells(2).Style.BackColor = Color.Thistle
+                    End If
+            End Select
+            r.Cells(5).Style.BackColor = mainForm.color_belimlight
+            r.Cells(6).Style.BackColor = mainForm.color_PRLighting
+            r.Cells(7).Style.BackColor = mainForm.color_blackout
+            r.Cells(8).Style.BackColor = mainForm.color_vision
+            r.Cells(9).Style.BackColor = mainForm.color_stage
+
+            _DGV.Columns(21).Visible = False
+
+            r.Cells(22).Style.BackColor = mainForm.color_belimlight
+            'r.Cells(22).Value = 0
+            r.Cells(23).Style.BackColor = mainForm.color_PRLighting
+            'r.Cells(23).Value = 0
+            r.Cells(24).Style.BackColor = mainForm.color_blackout
+            'r.Cells(24).Value = 0
+            r.Cells(25).Style.BackColor = mainForm.color_vision
+            'r.Cells(25).Value = 0
+            r.Cells(26).Style.BackColor = mainForm.color_stage
+            'r.Cells(26).Value = 0
+
+
+        Next r
+
+
+
+        _DGV.Columns(1).Width = 30
+        _DGV.Columns(1).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter                      ' Cat
+        _DGV.Columns(2).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        _DGV.Columns(2).MinimumWidth = 80
+        _DGV.Columns(2).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(2).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)           ' ID
+        _DGV.Columns(3).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        _DGV.Columns(3).MinimumWidth = 430
+        _DGV.Columns(3).DefaultCellStyle.BackColor = Color.FromArgb(242, 245, 245)
+        _DGV.Columns(3).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)           ' Fixture
+        _DGV.Columns(4).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        _DGV.Columns(4).MinimumWidth = 50
+        _DGV.Columns(4).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)           ' Q-ty
+        _DGV.Columns(4).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(5).Visible = True                                                                             ' BelImlight
+        _DGV.Columns(5).Width = 50
+        _DGV.Columns(5).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)
+        _DGV.Columns(5).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(6).Visible = True                                                                             ' PRLightigTouring
+        _DGV.Columns(6).Width = 50
+        _DGV.Columns(6).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)
+        _DGV.Columns(6).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(7).Visible = True                                                                             ' BlackOut
+        _DGV.Columns(7).Width = 50
+        _DGV.Columns(7).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)
+        _DGV.Columns(7).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(8).Visible = True                                                                             ' Vision
+        _DGV.Columns(8).Width = 50
+        _DGV.Columns(8).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)
+        _DGV.Columns(8).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(9).Visible = True                                                                             ' Stage
+        _DGV.Columns(9).Width = 50
+        _DGV.Columns(9).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)
+        _DGV.Columns(9).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(10).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        _DGV.Columns(10).MinimumWidth = 60
+        _DGV.Columns(10).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(10).Visible = False          ' Weight
+        _DGV.Columns(11).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        _DGV.Columns(11).MinimumWidth = 68
+        _DGV.Columns(11).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(11).Visible = False          ' Power
+        _DGV.Columns(12).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        _DGV.Columns(12).MinimumWidth = 60
+        _DGV.Columns(12).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(12).Visible = False          ' Price
+
+        _DGV.Columns(12).DefaultCellStyle.Format = NRFormat
+
+        _DGV.Columns(13).Visible = False                                                                            ' OrderQty
+        _DGV.Columns(14).Visible = False                                                                            ' R2
+        _DGV.Columns(15).Visible = False                                                                            ' R3
+        _DGV.Columns(16).Visible = False                                                                            ' R4          
+        _DGV.Columns(17).Visible = False                                                                            ' R5
+        _DGV.Columns(18).Visible = False                                                                            ' R6
+        _DGV.Columns(19).Visible = False                                                                            ' R7
+        _DGV.Columns(20).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        _DGV.Columns(20).MinimumWidth = 60
+        _DGV.Columns(20).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(20).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)          ' R8
+
+        _DGV.Columns(21).Visible = True                                                                             ' BelImlight
+        _DGV.Columns(21).Width = 50
+        _DGV.Columns(21).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)
+        _DGV.Columns(21).DefaultCellStyle.ForeColor = Color.Red
+        _DGV.Columns(21).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(22).Visible = True                                                                             ' BelImlight
+        _DGV.Columns(22).Width = 50
+        _DGV.Columns(22).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)
+        _DGV.Columns(22).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(23).Visible = True                                                                             ' PRLightigTouring
+        _DGV.Columns(23).Width = 50
+        _DGV.Columns(23).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)
+        _DGV.Columns(23).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(24).Visible = True                                                                             ' BlackOut
+        _DGV.Columns(24).Width = 50
+        _DGV.Columns(24).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)
+        _DGV.Columns(24).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(25).Visible = True                                                                             ' Vision
+        _DGV.Columns(25).Width = 50
+        _DGV.Columns(25).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)
+        _DGV.Columns(25).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        _DGV.Columns(26).Visible = True                                                                             ' Stage
+        _DGV.Columns(26).Width = 50
+        _DGV.Columns(26).DefaultCellStyle.Font = New Font("Calibri", 11, FontStyle.Bold, FontStyle.Italic)
+        _DGV.Columns(26).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+
+        ' _DGV.Rows(_DGV.Rows.Count - 1).Visible = False
+        _DGV.ClearSelection()
+    End Sub
 End Module
